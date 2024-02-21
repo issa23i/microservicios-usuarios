e3b6b6f26e01d8b4d8e7f00b1f6eee46ed63efea
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-02-21 17:47:50 +0100

    CRUD

---
+++ src/main/java/com/nttdata/microservicios/app/usuarios/services/AlumnoService.java
@@ -0,0 +1,12 @@
+package com.nttdata.microservicios.app.usuarios.services;
+
+import java.util.Optional;
+
+import com.nttdata.microservicios.app.usuarios.models.entity.Alumno;
+
+public interface AlumnoService {
+	public Iterable<Alumno> findAll();
+	public Optional<Alumno> findById(Long id);
+	public Alumno save(Alumno alumno);
+	public void deleteById(Long id);
+}
