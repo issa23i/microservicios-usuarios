8073cf31c8a4dcb0f15ca6d1c65fc1ddab0d8d0d
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-02-22 14:58:33 +0100

    Cliente Eureka

--- src/main/java/com/nttdata/microservicios/app/usuarios/MicroserviciosUsuariosApplication.java
+++ src/main/java/com/nttdata/microservicios/app/usuarios/MicroserviciosUsuariosApplication.java
@@ -3,6 +3,7 @@ package com.nttdata.microservicios.app.usuarios;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MicroserviciosUsuariosApplication {
 
