ab7cc854bb64b0a9b7ecf20fd3e7f757212425d0
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-02-23 13:19:59 +0100

    servicios , controlador

--- src/main/java/com/nttdata/microservicios/app/usuarios/services/AlumnoService.java
+++ src/main/java/com/nttdata/microservicios/app/usuarios/services/AlumnoService.java
@@ -1,8 +1,8 @@
 package com.nttdata.microservicios.app.usuarios.services;
 
-
 import com.nttdata.microservicios.app.usuarios.models.entity.Alumno;
 import com.nttdata.microservicios.commons.services.CommonService;
 
-public interface AlumnoService extends CommonService<Alumno>{
+public interface AlumnoService extends CommonService<Alumno> {
+
 }
