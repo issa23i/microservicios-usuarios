7cd44d021eb2ae4435f8456caead736118241e84
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-02-27 17:54:03 +0100

    subir imagenes

--- src/main/java/com/nttdata/microservicios/app/usuarios/controllers/AlumnoController.java
+++ src/main/java/com/nttdata/microservicios/app/usuarios/controllers/AlumnoController.java
@@ -1,5 +1,6 @@
 package com.nttdata.microservicios.app.usuarios.controllers;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
@@ -7,9 +8,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.nttdata.microservicios.commons.alumnos.models.entity.Alumno;
 import com.nttdata.microservicios.app.usuarios.services.AlumnoService;
@@ -37,10 +41,36 @@ public class AlumnoController extends CommonController<Alumno, AlumnoService>{
 		alumnoDb.setEmail(alumno.getEmail());
 		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(alumnoDb));
 	}
-	
+	@PutMapping("/editar-con-foto/{id}")
+	public ResponseEntity<?> editarConFoto(@Valid Alumno alumno, BindingResult result, @PathVariable Long id, @RequestParam MultipartFile archivo) throws IOException{
+		if(result.hasErrors()) {
+			return this.validar(result);
+		}
+		Optional<Alumno> o = service.findById(id);
+		if(o.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		Alumno alumnoDb = o.get();
+		alumnoDb.setNombre(alumno.getNombre());
+		alumnoDb.setApellido(alumno.getApellido());
+		alumnoDb.setEmail(alumno.getEmail());
+		if(!archivo.isEmpty()) {
+			alumnoDb.setFoto(archivo.getBytes());
+		}
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(alumnoDb));
+	}
 	@GetMapping("/filtrar/{term}")
 	public ResponseEntity<?> filtrar(@PathVariable String term){
 		return ResponseEntity.ok(service.findByNombreOApellido(term));
+	}
+
+	@PostMapping("/crear-con-foto")
+	public ResponseEntity<?> crearConFoto(@Valid Alumno alumno, BindingResult result, @RequestParam MultipartFile archivo) throws IOException {
+		if(!archivo.isEmpty()) {
+			alumno.setFoto(archivo.getBytes());
+		}
+		return super.crear(alumno, result);
 	}
 
 }
