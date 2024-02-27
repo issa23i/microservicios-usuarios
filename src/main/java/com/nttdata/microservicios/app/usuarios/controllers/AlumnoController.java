5113b9fc22b5890b9187796978f5ac68d3a09ce7
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-02-27 17:02:58 +0100

    Validaciones y paginación

--- src/main/java/com/nttdata/microservicios/app/usuarios/controllers/AlumnoController.java
+++ src/main/java/com/nttdata/microservicios/app/usuarios/controllers/AlumnoController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,12 +15,17 @@ import com.nttdata.microservicios.commons.alumnos.models.entity.Alumno;
 import com.nttdata.microservicios.app.usuarios.services.AlumnoService;
 import com.nttdata.microservicios.commons.controllers.CommonController;
 
+import jakarta.validation.Valid;
+
 @RestController
 public class AlumnoController extends CommonController<Alumno, AlumnoService>{
 
 
 	@PutMapping("/{id}")
-	public ResponseEntity<?> editar(@RequestBody Alumno alumno, @PathVariable Long id){
+	public ResponseEntity<?> editar(@Valid @RequestBody Alumno alumno, BindingResult result, @PathVariable Long id){
+		if(result.hasErrors()) {
+			return this.validar(result);
+		}
 		Optional<Alumno> o = service.findById(id);
 		if(o.isEmpty()) {
 			return ResponseEntity.notFound().build();
